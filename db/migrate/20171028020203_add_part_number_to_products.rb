28b36a6c18968eac680866f31b4ea955d195293a
jeremymary <jeremyjohnmary@gmail.com>
2017-10-27 19:44:28 -0700

    finally finished

---
+++ db/migrate/20171028020203_add_part_number_to_products.rb
@@ -0,0 +1,10 @@
+class AddPartNumberToProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :level
+      t.integer :trainer_id
+
+    end
+  end
+end
